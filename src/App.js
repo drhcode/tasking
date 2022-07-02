d8fc1f67071d4547dcda576c8ac4b7afbfaf26d8
drhcode <drhcode1@gmail.com>
2022-07-02 19:54:26 +0200

    fixed some changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
+import Footer from "./components/Footer";
+import About from "./components/About";
 
 const App = () => {
   const [addTaskModal, setAddTaskModal] = useState(false);
   const [tasks, setTasks] = useState([]);
 
-  const tasksData = "http://localhost:5000/tasks";
+  const tasksData = "http://localhost:5000/tasks/";
 
   useEffect(() => {
     const getTasks = async () => {
@@ -25,23 +28,56 @@ const App = () => {
     return data;
   };
 
+  // GET A TASK FROM SERVER
+  const fetchTask = async (id) => {
+    const response = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await response.json();
+
+    return data;
+  };
+
   // Add new task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1;
-    const newTask = { id, ...task };
-    setTasks([...tasks, newTask]);
+  const addTask = async (task) => {
+    const res = await fetch(`http://localhost:5000/tasks`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
+    const data = await res.json();
+
+    setTasks([...tasks, data]);
+
+    // const id = Math.floor(Math.random() * 10000) + 1;
+    // const newTask = { id, ...task };
+    // setTasks([...tasks, newTask]);
   };
 
   // Remove Tasks
-  const removeTask = (id) => {
+  const removeTask = async (id) => {
+    await fetch(`tasksData/${id}`, { method: "DELETE" });
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
   // Reminder for tasks that
-  const taskReminder = (id) => {
+  const taskReminder = async (id) => {
+    const taskToRemind = await fetchTask(id);
+    const updatedTask = { ...taskToRemind, reminder: !taskToRemind.reminder };
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(updatedTask),
+    });
+
+    const data = await res.json();
+
     setTasks(
       tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
+        task.id === id ? { ...task, reminder: data.reminder } : task
       )
     );
   };
@@ -59,6 +95,9 @@ const App = () => {
       ) : (
         "No task to do"
       )}
+
+      <Footer />
+      <About />
     </div>
   );
 };
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        Copyright 2022 - Made by <a href="https://drhcode.com">DRHCode</a>
+      </p>
+      <span>
+        <a href="/about">About</a>
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
